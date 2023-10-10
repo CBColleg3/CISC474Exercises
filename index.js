671af3cfe0843a1dacab2f77c0007128eddfc597
Christopher Bennett <cbcolleg@udel.edu>
2023-10-10 08:29:07 -0400

    exercises added

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    /*
+    document.getElementById("go").onclick = () => {
+		console.log("click is running! Lets Go");
+		document.getElementById("fname").style.display = "none";
+		document.getElementById("lname").style.display = "";
+
+        document.getElementById("jumbotron")
+	}
+    */
+
+    $('#go').click(function(){
+        //Some code
+        console.log("submit button clicked");
+    });
 });
