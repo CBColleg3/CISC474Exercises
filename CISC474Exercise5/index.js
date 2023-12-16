75cf0bd300fbedfce7016ebd4f69e774f48b686b
Christopher Bennett <cbcolleg@udel.edu>
2023-12-15 21:39:45 -0500

    moved exercise5 over

--- CISC474Exercise5/index.js
+++ CISC474Exercise5/index.js
@@ -1,3 +1,28 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("/etc/secrets/fbdemo2-ebe0a-firebase-adminsdk-box2t-ffef60650b.json");
+
+const { getDatabase } = require('firebase-admin/database');
+const db = getDatabase();
+const ref = db.ref('server/saving-data/');
+const usersRef = ref.child('users');
+usersRef.set({
+alanisawesome: {
+date_of_birth: 'June 23, 1912',
+full_name: 'Alan Turing'
+},
+gracehop: {
+date_of_birth: 'December 9, 1906',
+full_name: 'Grace Hopper'
+}
+});
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://fbdemo2-ebe0a-default-rtdb.firebaseio.com"
+});
+
 const http = require('http')
 const port = 8080
 // Create a server object:
